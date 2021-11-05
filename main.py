bcaacf57bdaade8b5b60f4494b03ea689b8a07be
Logan Tracy <loganstracy@gmail.com>
2021-11-05 02:40:07 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import pygame
+from pygame import draw
+from sort import SortContainer
+from time import sleep
+
+FPS = 10
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
+
+BAR_WIDTH = 3
+BAR_HEIGHT_MULT = 2
+BAR_SPACING = 2
+
+pygame.init()
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Sort Visualizer')
+clock = pygame.time.Clock()
+
+items = SortContainer()
+
+def draw_elements(snapshot, last_touched=None):
+    for count, element in enumerate(snapshot):
+        pygame.draw.rect(window, (255,255,255), pygame.Rect((BAR_SPACING+BAR_WIDTH)*count, 0, BAR_WIDTH, element*BAR_HEIGHT_MULT))
+
+running = True
+sorting = False
+idx = 0
+while running:
+    inputs = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r and not sorting:
+                items.shuffle_elements()
+                idx = 0
+            if event.key == pygame.K_SPACE:
+                if not sorting:
+                    if idx == 0:
+                        sorting = True
+    
+    # delta_time = clock.tick(FPS)
+    clock.tick(FPS)
+    # sleep(1/FPS)
+
+    window.fill((0,0,0))
+
+    if sorting and idx < len(items.elements): # TODO: figure out why display is already sorted on first frame
+        draw_elements(items.get_snapshot(idx))
+        idx += 1
+    else:
+        draw_elements(items.get_snapshot(idx))
+        sorting = False
+
+    pygame.display.flip()
+
+pygame.quit()
---
+++ sort.py
@@ -0,0 +1,71 @@
+from random import randint, shuffle
+
+class SortContainer():
+    def __init__(self, size=300, type='insertion'):
+        self.size = size
+        self.elements = self.new_elements()
+        self.snapshot = []
+        self.sort_type = type
+        self.change_sort(self.sort_type)
+    
+    def new_elements(self):
+        e = [x+1 for x in range(self.size)]
+        shuffle(e)
+        return e
+
+    def shuffle_elements(self):
+        shuffle(self.elements)
+    
+    def add_snapshot(self, custom=None):
+        if custom == None:
+            self.snapshot.append(self.elements)
+        else:
+            self.snapshot.append(custom)
+    
+    def clear_snapshot(self):
+        self.snapshot = []
+        self.snapshot.append(self.elements)
+    
+    def get_snapshot(self, index):
+        return self.snapshot[index]
+    
+    def change_sort(self, type):
+        self.sort_type == type
+
+        if self.sort_type == 'insertion':
+            self.insertion_sort()
+    
+    def insertion_sort(self):
+        self.clear_snapshot()
+        buffer = self.elements
+        self.add_snapshot(buffer)
+        for i in range(1, len(buffer)):
+            key_item = buffer[i]
+
+            j = i - 1
+
+            while j >= 0 and buffer[j] > key_item:
+                buffer[j + 1] = buffer[j]
+                j -= 1
+                self.add_snapshot(buffer)
+            
+            buffer[j + 1] = key_item
+
+
+def quick_sort(elements):
+    if len(elements) < 2:
+        return elements
+    
+    low, same, high = [], [], []
+
+    pivot = elements[randint(0, len(elements) - 1)]
+
+    for item in elements:
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+    
+    return quick_sort(low) + same + quick_sort(high)
